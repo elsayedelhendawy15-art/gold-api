11ff326f10a9071e07de970009635b6e12aa9854
elsayedelhendawy15-art <elsayedelhendawy15@gmail.com>
2026-05-09 02:13:30 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,27 +1,11 @@
-import requests
 import json
-from datetime import datetime
 
-def get_gold_data():
-    try:
-        # جلب السعر العالمي فقط للتجربة والتأكد من نجاح الـ API
-        global_url = "https://api.binance.com/api/v3/ticker/price?symbol=PAXGUSDT"
-        global_res = requests.get(global_url, timeout=10)
-        ounce_usd = float(global_res.json()['price'])
-        
-        data = {
-            "last_update": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-            "ounce_usd": ounce_usd,
-            "status": "online"
-        }
+data = {
+    "price": "3600",
+    "status": "test_success"
+}
 
-        with open('gold_data.json', 'w', encoding='utf-8') as f:
-            json.dump(data, f, ensure_ascii=False, indent=4)
-        
-        print("Success: File Created!")
+with open('gold_data.json', 'w') as f:
+    json.dump(data, f)
 
-    except Exception as e:
-        print(f"Error: {e}")
-
-if __name__ == "__main__":
-    get_gold_data()
+print("File Created Successfully!")
